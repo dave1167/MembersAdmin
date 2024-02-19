b03256e48b19ed721826b63788a6600e3edf51ae
dave1167 <daverowe1167@gmail.com>
2024-02-19 08:08:45 +0000

    ll

--- public/dmxAppConnect/config.js
+++ public/dmxAppConnect/config.js
@@ -1,54 +1,13 @@
 dmx.config({
   "members": {
-    "query": [
-      {
-        "type": "text",
-        "name": "sort"
-      },
-      {
-        "type": "text",
-        "name": "dir"
-      }
-    ],
-    "lastPayRecDataStore": [
-      {
-        "type": "number",
-        "name": "payType"
-      }
-    ],
-    "sessionStorage": [
-      {
-        "type": "number",
-        "name": "payType"
-      },
-      {
-        "type": "text",
-        "name": "lastDatePaid"
-      },
-      {
-        "type": "number",
-        "name": "memDetesID"
-      },
-      {
-        "type": "number",
-        "name": "payID"
-      }
-    ]
-  },
-  "test1": {
-    "sessionStorage": [
-      {
-        "type": "text",
-        "name": "var"
-      }
-    ]
-  },
-  "test3": {
-    "sessionStorage": [
-      {
-        "type": "text",
-        "name": "var"
-      }
-    ]
+    "flow1": {
+      "meta": [
+        {
+          "name": "payDueDate",
+          "type": "text"
+        }
+      ],
+      "local": {}
+    }
   }
 });
